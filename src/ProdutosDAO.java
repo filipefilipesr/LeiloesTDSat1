e9e1c73d6bf751d5d544867534f60dc8e7507a20
Filipe Resende <filiperesendesales2007@gmail.com>
2024-07-08 20:53:59 -0300

    Cadastro de itens

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,11 +23,25 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public int cadastrarProduto (ProdutosDTO produto){
         
         
-        //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB();
         
+        int status;
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)");
+            
+            prep.setString(1,produto.getNome());
+            prep.setInt(2,produto.getValor());
+            prep.setString(3,produto.getStatus());
+            
+            status = prep.executeUpdate();
+            return status; //retornar 1
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            return ex.getErrorCode();
+        }
         
     }
     
